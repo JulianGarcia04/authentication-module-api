15d4e5655f7b779dc3dfa1c84666df01a0d1f801
Julián Camilo García Rincón <jcgrrincon@gmail.com>
2024-09-16 18:50:17 -0500

    add coerce prop to EnvSchema

--- src/env.ts
+++ src/env.ts
@@ -5,5 +5,5 @@ export const EnvSchema = z.object({
   TWILIO_AUTH_TOKEN: z.string(),
   TWILIO_VERIFY_SERVICE: z.string(),
   JWT_SECRET: z.string(),
-  PORT: z.number(),
+  PORT: z.coerce.number(),
 });
